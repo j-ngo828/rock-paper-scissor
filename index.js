93db5caad039fe64a1c6692e063f28cd990725f4
Jack Ngo <83608215+j-ngo828@users.noreply.github.com>
2023-03-01 21:29:38 -0500

    Add final logic

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function getComputerChoice() {
   }
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection, state) {
   const playerChoice = playerSelection.toLowerCase();
   if (playerChoice === computerSelection) {
     return "TIE!";
@@ -23,44 +23,51 @@ function playRound(playerSelection, computerSelection) {
 
   if (playerChoice === ROCK) {
     if (computerSelection === PAPER) {
+      state.computerWon += 1;
       return "You Lose! Paper beats Rock";
     } else {
+      state.playerWon += 1;
       return "You Win! Rock beats Scissor";
     }
   } else if (playerChoice === PAPER) {
     if (computerSelection === ROCK) {
+      state.playerWon += 1;
       return "You Win! Paper beats Rock";
     } else {
+      state.computerWon += 1;
       return "You Lose! Scissor beats Paper";
     }
   } else {
     if (computerSelection === ROCK) {
+      state.computerWon += 1;
       return "You Lose! Rock beats Scissor";
     } else {
+      state.playerWon += 1;
       return "You Win! Scissor beats Paper";
     }
   }
 }
 
-function game() {
-  const round = 0;
-  for (let i = 1; i === 1; ++i) {
-    const playerSelection = prompt("Enter your choice: ");
-    const computerSelection = getComputerChoice();
-    console.log(
-      `Game ${i} result: ${playRound(playerSelection, computerSelection)}`
-    );
-  }
-}
-
 function main() {
   const choiceButtons = document.querySelectorAll(".choice");
-  const result = document.querySelector(".result");
+  const roundResult = document.querySelector(".round-result");
   const playerChoiceElem = document.querySelector(".my-choice");
   const computerChoiceElem = document.querySelector(".computer-choice");
   const roundElem = document.querySelector(".round-counter");
+  const container = document.querySelector(".container");
+  const finalResult = document.createElement("div");
+  const playerWonElem = document.querySelector(".player-won");
+  const computerWonElem = document.querySelector(".computer-won");
+  finalResult.classList.add("final-result");
   let round = 1;
+  const state = {
+    playerWon: 0,
+    computerWon: 0,
+  };
+  // let computerWon = 0;
   roundElem.textContent = `Round: ${round}`;
+  playerWonElem.textContent = `You won: ${state.playerWon} round`;
+  computerWonElem.textContent = `Computer won: ${state.computerWon} round`;
   const formatChoice = (choice) =>
     `${choice[0].toUpperCase()}${choice.slice(1)}`;
   const handleClick = (event) => {
@@ -73,8 +80,20 @@ function main() {
     computerChoiceElem.textContent = `Computer chose ${formatChoice(
       computerChoice
     )}`;
-    const roundResult = playRound(playerChoice, computerChoice);
-    result.textContent = roundResult;
+    const roundOutcome = playRound(playerChoice, computerChoice, state);
+    roundResult.textContent = roundOutcome;
+    playerWonElem.textContent = `You won: ${state.playerWon} round`;
+    computerWonElem.textContent = `Computer won ${state.computerWon} round`;
+    if (state.playerWon === 5 || state.computerWon === 5) {
+      finalResult.textContent = `Winner is: ${
+        state.playerWon === 5 ? "you!!" : "the computer!!"
+      }`;
+      container.appendChild(finalResult);
+      // round = 1;
+      // roundElem.textContent = `Round: ${round}`;
+      state.playerWon = 0;
+      state.computerWon = 0;
+    }
   };
   choiceButtons.forEach((button) =>
     button.addEventListener("click", handleClick)
